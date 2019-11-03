f909c7b70d4b758cad8d4747d5ecbaf9bf6ac951
pcy <2674467254@qq.com>
2019-11-03 19:34:47 +0800

    commit

---
+++ cprimer/printf/longstr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    printf("here is one way to print a ");
+    printf("long string.\n");
+
+    printf("here is another way to print a\
+    long string.\n");
+
+    printf("here is the newest way to print a "
+    "long string.\n");
+
+    printf("hello,young lovers,wherever you are.\n");
+
+    printf("hello,young " "lovers" ",wherever you are.\n");
+
+    printf("hello,young lovers"
+    ",wherever you are.\n");
+    system("pause");
+    return 0;
+}

855fb5f8534e8a536ba76621e82871a1f2340881
pcy <2674467254@qq.com>
2019-11-03 13:32:07 +0800

    commit

---
+++ scanf/way2.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+int main()
+{
+	int i;
+	char ch;
+
+	scanf("%d", &i);
+	printf("i = %d\n", i);
+
+
+	while((ch=getchar())!= EOF && ch !='\n'){
+    ;
+	}
+	int j;
+	scanf("%d", &j);
+        printf("j = %d\n", j);
+
+    system("pause");
+	return 0;
+}

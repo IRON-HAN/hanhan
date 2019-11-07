5f921a99648afb0f1241837176786fa828443a77
pcy <2674467254@qq.com>
2019-11-07 11:26:03 +0800

    commit

--- homework/for1.c
+++ homework/for1.c
@@ -3,14 +3,11 @@ int main() {
   int a, b, c, amount;
   amount = 10;
 
-  c = 0;
-  while (c<=2) {
+
+  for (c=0; c<=2; c++) {
     amount = amount - c*5;
-    c++;
-    b = 0;
-    while (b<=(amount/2)) {
+    for (b=0; b<=(amount/2); b++) {
       a = amount - b * 2;
-      b++;
       printf("一角=%d，贰角=%d，伍角=%d ", a, b, c);
       printf("\n");
     }

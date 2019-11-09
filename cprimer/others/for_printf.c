1eb5f4be430987faf5b801066d25646ddb63eaed
pcy <2674467254@qq.com>
2019-11-09 20:30:48 +0800

    commit

---
+++ cprimer/others/for_printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int num = 0;
+    for (printf("keep entering nums!\n"); num != 520;) {
+      scanf("%d", &num);
+    }
+    printf("thats the num i want!\n");
+
+    //getchar();
+    system("pause");
+    return 0;
+}

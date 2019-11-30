612d308e8d552be6546eed20c68687e86375451a
pcy <2674467254@qq.com>
2019-11-30 17:18:22 +0800

    commit

--- homework/C8/T2.C
+++ homework/C8/T2.C
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 int main() {
+    void AT(int **a, int n);
     int row = 0;
     int col = 0;
     int i;
@@ -13,19 +14,40 @@ int main() {
     arr = (int **)malloc(sizeof(int *) * row);
     for (i=0; i<row; i++) {
         arr[i] = (int *)malloc(sizeof(int) * col);
+    }//arr[r][c]
+    printf("please input:\n");
+    for (int r=0; r<row; r++) {
+        for (int c=0; c<col; c++) {
+            scanf("%d", &arr[r][c]);
+    }
+    }
+    for (int r=0; r<row; r++) {
+        for (int c=0; c<col; c++) {
+            printf("%-5d", arr[r][c]);
+    }
+    printf("\n");
+    }
+    AT(arr, row);
+    for (int r=0; r<row; r++) {
+        for (int c=0; c<col; c++) {
+            printf("%-5d", arr[r][c]);
+    }
+    printf("\n");
     }
 
     //getchar();
-    //system("pause");
+    system("pause");
     return 0;
 }
-void at(int *a, int n){
+
+void AT(int **a, int n){
     int i, j, t;
     for (i=0; i<n; i++) {
-    for (j=i+1; j<n; j++) {
-        t = a[i * n + j];
-        a[i * n + j] = a[j * n + i];
-        a[j * n + i] = t;
+        for (j=i+1; j<n; j++) {
+            t = a[i][j];
+            a[i][j] = a[j][i];
+            a[j][i] = t;
     }
     }
 }
+

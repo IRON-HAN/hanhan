e4a5f2806ed35940f367d6985668b25976863ad9
pcy <2674467254@qq.com>
2019-12-26 19:59:28 +0800

    commit

--- 1测试/1-test.c
+++ 1测试/1-test.c
@@ -5,13 +5,6 @@
 #include <stdlib.h>
 #include <time.h>
 int main() {
-    void *p;
-    int cnt = 0;
-    while ((p=malloc(100*1024*1024))) {
-        cnt++;
-    }
-    printf("%d00MB\n", cnt);
-    free(p);
 
     system("pause");
     return 0;

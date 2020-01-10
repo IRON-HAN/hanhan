83b042e44f86c4f7ff0be26d81efb942622c4006
pcy <2674467254@qq.com>
2020-01-10 09:07:56 +0800

    commit

--- CPP-codes/1-test.cpp
+++ CPP-codes/1-test.cpp
@@ -2,12 +2,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-    freopen("E:\\VS-Code-C\\CPP-codes\\test.txt", "r", stdin);
-    int n, max;
-    while (cin >> n) {
-        if(n > max)
-            max = n;
-    }
-    cout << max << endl;
+
     return 0;
 }
